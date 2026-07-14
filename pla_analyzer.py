03b364a4ccc713966c6d0651d44253da89115f75
Joanna <joanna.jansi@gmail.com>
2026-07-14 11:37:41 -0400

    Add image loading

--- pla_analyzer.py
+++ pla_analyzer.py
@@ -15,24 +15,13 @@ from skimage import filters, measure, segmentation, morphology, color
 from skimage.io import imread, imsave
 from scipy import ndimage as ndi
 
-def segment_nuclei(image):
-    blurred = filters.gaussian(image, sigma=2)
-    thresh = filters.threshold_otsu(blurred)
-    binary = blurred > thresh
-    labeled = measure.label(binary)
-    return labeled
-
-def count_puncta(pla_image, labeled_nuclei):
-    blurred = filters.gaussian(pla_image, sigma=1)
-    thresh = filters.threshold_otsu(blurred)
-    puncta_mask = blurred > thresh
-    puncta_labeled = measure.label(puncta_mask)
-    counts = {}
-    for nucleus_id in range(1, labeled_nuclei.max() + 1):
-        nucleus_mask = labeled_nuclei == nucleus_id
-        dots_in_nucleus = puncta_labeled[nucleus_mask]
-        counts[nucleus_id] = len(np.unique(dots_in_nucleus)) - 1
-    return counts
-
-def make_plot(counts, cell_type)
-    pass
+def load_image(path: str) -> np.ndarray:
+    img = imread(path)
+    if img.ndim == 3 and img.shape[2] not in (3, 4):
+        img = img.max(axis=0)
+    if img.ndim == 3 and img.shape[2] in (3, 4):
+        img = color.rgb2gray(img[..., :3])
+        img = (img * 255).astype(np.float32)
+    else:
+        img = img.astype(np.float32)
+    return img
